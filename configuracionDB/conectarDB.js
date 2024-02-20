677c26f2511d531627ea307513f4399be0659aec
victor <121758884+vitgnomax@users.noreply.github.com>
2024-02-20 19:06:14 +0100

    Update conectarDB.js

--- configuracionDB/conectarDB.js
+++ configuracionDB/conectarDB.js
@@ -4,13 +4,13 @@ const pgp = require('pg-promise')(initOptions)
 
 require('dotenv').config()
 
-const cn = {
+/*const cn = {
     user: process.env.DEBUG_USER,
     password: process.env.DEBUG_PASSWORD,
     host:'localhost',
     port: 5432,
     database: 'postgres'
-}
+}*/
 
 //const db = pgp(cn)
 //editado
