45aedc6800e14a03ca11e999e2a1a39eaa7e51c0
victor <121758884+vitgnomax@users.noreply.github.com>
2024-02-20 13:07:34 +0100

    Update app.js 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/metas');
+//var usersRouter = require('./routes/metas');
 
 var app = express();
 
