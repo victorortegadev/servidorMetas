342e778577c98160c70f0f71a73019fb01ecce0d
vitmax <victoreuro11@gmail.com>
2024-02-22 13:40:55 +0100

    cors activo mediante  middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/metas');
@@ -17,6 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors())
 
 app.use('/hola3', indexRouter);
 app.use('/api', usersRouter);
