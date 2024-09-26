2f351ede69d79063eb55ab29e7be5bd794bba265
vitmax <victoreuro11@gmail.com>
2024-09-26 21:23:29 +0200

    modificación de la conexión a la base de datos

--- configuracionDB/conectarDB.js
+++ configuracionDB/conectarDB.js
@@ -4,21 +4,22 @@ const pgp = require('pg-promise')(initOptions)
 
 require('dotenv').config()
 
-/*const cn = {
-    user: process.env.DEBUG_USER,
-    password: process.env.DEBUG_PASSWORD,
-    host:'localhost',
-    port: 5432,
-    database: 'postgres'
-}*/
-
-//const db = pgp(cn)
-//editado
-
 const cn = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+}
+
+const db = pgp(cn)
+
+
+/*const cn = {
     connectionString: process.env.DATABASE_URL ,
     ssl:true,
     extra:{ssl: {rejectUnauthorized: false}}
 };
-const db = pgp(cn)
+const db = pgp(cn)*/
+
 module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('hola3');
+  res.send('hola6');
 });
 
 module.exports = router;
